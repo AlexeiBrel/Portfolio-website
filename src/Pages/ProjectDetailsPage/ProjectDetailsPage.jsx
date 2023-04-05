d8b2f0fd8bfd5014887cd5b1dc4f8f6358209b3e
Alexey Brel <brel.2000@mail.ru>
2023-04-05 12:33:28 +0300

    adding new projects

--- src/Pages/ProjectDetailsPage/ProjectDetailsPage.jsx
+++ src/Pages/ProjectDetailsPage/ProjectDetailsPage.jsx
@@ -7,8 +7,11 @@ import cl from './ProjectDetailsPage.module.scss';
 const ProjectDetailsPage = () => {
     const { id } = useParams();
     const [project, setProject] = useState([
-        { id: 1, title: 'Aperture', picture: '/img/projects/02-2.png', pictureMobile: '/img/projects/01.png', technologies: 'HTML, CSS, JavaScript', gitlink: '#' },
-        { id: 2, title: 'Quiz', picture: '/img/projects/02-2.png', pictureMobile: '/img/projects/02.png', technologies: 'ReactJS, Node.js', gitlink: '#' },
+        { id: 1, title: 'Aperture', picture: '/img/projects/01-1.png', pictureMobile: '/img/projects/01.png', technologies: 'HTML, CSS, JavaScript', gitlink: 'https://github.com/AlexeiBrel/Aperture' },
+        { id: 2, title: 'Quiz', picture: '/img/projects/02-2.png', pictureMobile: '/img/projects/02.png', technologies: 'HTML, CSS, SCSS, JS, jQuery', gitlink: 'https://github.com/AlexeiBrel/quiz' },
+        { id: 3, title: 'Спаси жизнь', picture: '/img/projects/03-3.png', pictureMobile: '/img/projects/03.png', technologies: 'HTML, CSS, SCSS, JS', gitlink: 'https://github.com/AlexeiBrel/Save-life' },
+        { id: 4, title: 'MBox', picture: '/img/projects/04-4.png', pictureMobile: '/img/projects/04.png', technologies: 'HTML, CSS, SCSS', gitlink: 'https://github.com/AlexeiBrel/M-Box-TV' },
+        { id: 5, title: 'Ремонт бытовой техники', picture: '/img/projects/05-5.png', pictureMobile: '/img/projects/05.png', technologies: 'HTML, CSS, SCSS, Bootstrap, JS, jQuery, ', gitlink: 'https://github.com/AlexeiBrel/Version' },
     ])
 
     return (
--- src/Pages/ProjectsPage/ProjectsPage.jsx
+++ src/Pages/ProjectsPage/ProjectsPage.jsx
@@ -8,6 +8,9 @@ const ProjectsPage = () => {
     const [projects, setProjects] = useState([
         { id: 1, title: 'Aperture', picture: '/img/projects/01.png' },
         { id: 2, title: 'Quiz', picture: '/img/projects/02.png' },
+        { id: 3, title: 'Save-life', picture: '/img/projects/03.png' },
+        { id: 4, title: 'MBox', picture: '/img/projects/04.png' },
+        { id: 5, title: 'Ремонт бытовой техники', picture: '/img/projects/05.png' },
     ])
 
     return (
